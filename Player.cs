9138b17285eb5e55ed2d99da479d23f454e686f0
Develorain <feoamnfgoamngdasd@users.noreply.github.com>
2016-06-22 22:02:00 -0400

    Added assets and draw to screen

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RainyStory
+{
+	public class Player
+	{
+		public Texture2D texture;
+
+		public Vector2 position;
+
+		public Player (Texture2D texture, Vector2 position)
+		{
+			this.texture = texture;
+			this.position = position;
+		}
+
+		public void update() {
+
+		}
+
+		public void draw(SpriteBatch spriteBatch) {
+			spriteBatch.Draw (texture, position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		}
+	}
+}
+
