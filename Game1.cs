5e8874cba0b43df86c01656ba44b0b283c5624cd
Develorain <feoamnfgoamngdasd@users.noreply.github.com>
2016-06-23 12:48:52 -0400

    Added Mono-game-Extended and physics objects

--- Game1.cs
+++ Game1.cs
@@ -13,20 +13,46 @@ namespace RainyStory
 		private GraphicsDeviceManager graphics;
 		private SpriteBatch spriteBatch;
 
-		private Player player;
+		private cpSpace space;
+		private cpShape ground;
 
+		private Player player;
 		private Texture2D mapTexture;
 
 		public Game1 ()
 		{
 			graphics = new GraphicsDeviceManager (this);
+			graphics.PreferredBackBufferWidth = 1280;
+			graphics.PreferredBackBufferHeight = 720;
+			graphics.ApplyChanges();
 			Content.RootDirectory = "Content";
 		}
 
 		protected override void Initialize ()
 		{
-			// TODO: Add your initialization logic here
-			player = new Player(Content.Load<Texture2D>("character"), new Vector2(0, 0));
+			// Create space and initialize gravity
+			space = new cpSpace();
+			space.SetGravity (new cpVect (0, -100));
+
+			// Create ground and set friction and add to space
+			ground = new cpSegmentShape (space.GetStaticBody (), new cpVect (-200, 0), new cpVect (200, 0), 0);
+			ground.SetFriction (1);
+			space.AddShape (ground);
+
+			// Create radius, mass, moment of inertia of ball
+			float radius = 5;
+			float mass = 1;
+			float moment = cp.MomentForCircle (mass, 0, radius, cpVect.Zero);
+
+			// Create ball rigid body and set position
+			cpBody ballBody = space.AddBody (new cpBody (mass, moment));
+			ballBody.SetPosition (new cpVect (0, 10));
+
+			// Create collision shape and set friction
+			cpShape ballShape = space.AddShape(new cpCircleShape(ballBody, radius, cpVect.Zero));
+			ballShape.SetFriction (0.7f);
+
+			player = new Player(Content.Load<Texture2D>("character"), new Vector2(600, 450));
             
 			base.Initialize ();
 		}
@@ -53,12 +79,12 @@ namespace RainyStory
 
 		protected override void Draw (GameTime gameTime)
 		{
-			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+			graphics.GraphicsDevice.Clear (Color.DimGray);
             
 			//TODO: Add your drawing code here
 			spriteBatch.Begin();
 
-			spriteBatch.Draw (mapTexture, new Vector2(0, 65), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			spriteBatch.Draw (mapTexture, new Vector2(300, 550), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
 			player.draw (spriteBatch);
 
 			spriteBatch.End ();
