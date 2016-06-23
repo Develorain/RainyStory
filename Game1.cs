678817ed54027beab23aae4cfa2da9ff5310cb40
Develorain <feoamnfgoamngdasd@users.noreply.github.com>
2016-06-23 14:52:32 -0400

    Added debug rendering

--- Game1.cs
+++ Game1.cs
@@ -16,44 +16,48 @@ namespace RainyStory
 
 		private cpSpace space;
 		private cpShape ground;
+		private cpBody ballBody;
+		private cpShape ballShape;
 
 		private Player player;
 		private Texture2D mapTexture;
+		private float radius = 50f;
+		private cpVect pointA = new cpVect (300, -550);
+		private cpVect pointB = new cpVect (900, -550);
 
 		public Game1 ()
 		{
 			graphics = new GraphicsDeviceManager (this);
 			graphics.PreferredBackBufferWidth = 1280;
 			graphics.PreferredBackBufferHeight = 720;
-			graphics.ApplyChanges();
+			graphics.ApplyChanges ();
 			Content.RootDirectory = "Content";
 		}
 
 		protected override void Initialize ()
 		{
 			// Create space and initialize gravity
-			space = new cpSpace();
-			space.SetGravity (new cpVect (0, -100));
+			space = new cpSpace ();
+			space.SetGravity (new cpVect (0, -1200));
 
 			// Create ground and set friction and add to space
-			ground = new cpSegmentShape (space.GetStaticBody (), new cpVect (-200, 0), new cpVect (200, 0), 0);
+			ground = new cpSegmentShape (space.GetStaticBody (), pointA, pointB, 0);
 			ground.SetFriction (1);
 			space.AddShape (ground);
 
 			// Create radius, mass, moment of inertia of ball
-			float radius = 5;
 			float mass = 1;
 			float moment = cp.MomentForCircle (mass, 0, radius, cpVect.Zero);
 
 			// Create ball rigid body and set position
-			cpBody ballBody = space.AddBody (new cpBody (mass, moment));
-			ballBody.SetPosition (new cpVect (0, 10));
+			ballBody = space.AddBody (new cpBody (mass, moment));
+			ballBody.SetPosition (new cpVect (600, 0));
 
 			// Create collision shape and set friction
-			cpShape ballShape = space.AddShape(new cpCircleShape(ballBody, radius, cpVect.Zero));
+			ballShape = space.AddShape (new cpCircleShape (ballBody, radius, cpVect.Zero));
 			ballShape.SetFriction (0.7f);
 
-			player = new Player(Content.Load<Texture2D>("character"), new Vector2(600, 450));
+			player = new Player (Content.Load<Texture2D> ("character"), new Vector2 (600, 450));
             
 			base.Initialize ();
 		}
@@ -63,7 +67,7 @@ namespace RainyStory
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 
 			//TODO: use this.Content to load your game content here 
-			mapTexture = Content.Load<Texture2D>("map");
+			mapTexture = Content.Load<Texture2D> ("map");
 		}
 
 		protected override void Update (GameTime gameTime)
@@ -72,8 +76,8 @@ namespace RainyStory
 			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState ().IsKeyDown (Keys.Escape))
 				Exit ();
 			#endif
-            
-			// TODO: Add your update logic here
+
+			space.Step ((float)gameTime.ElapsedGameTime.TotalSeconds);
             
 			base.Update (gameTime);
 		}
@@ -83,12 +87,18 @@ namespace RainyStory
 			graphics.GraphicsDevice.Clear (Color.DimGray);
             
 			//TODO: Add your drawing code here
-			spriteBatch.Begin();
+			spriteBatch.Begin ();
 
-			spriteBatch.Draw (mapTexture, new Vector2(300, 550), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			spriteBatch.Draw (mapTexture, new Vector2 (300, 550), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
 			player.draw (spriteBatch);
 
-			SpriteBatchExtensions.DrawRectangle (spriteBatch, new RectangleF(0, 0, 100, 100), Color.Red, 1);
+			SpriteBatchExtensions.DrawLine (spriteBatch, Tools.toScreenVector2 (pointA), Tools.toScreenVector2 (pointB), Color.White, 1);
+
+			SpriteBatchExtensions.DrawCircle (spriteBatch,
+				new CircleF (Tools.toScreenVector2 (ballBody.GetPosition ()), Tools.toScreenFloat (radius)),
+				1000,
+				Color.Red,
+				1);
 
 			spriteBatch.End ();
             
@@ -96,4 +106,3 @@ namespace RainyStory
 		}
 	}
 }
-
---
+++ Tools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using ChipmunkSharp;
+
+namespace RainyStory
+{
+	public class Tools
+	{
+		public const int SCALE = 1;
+
+		public static Vector2 toScreenVector2 (cpVect vector)
+		{
+			return new Vector2 (vector.x * SCALE, vector.y * SCALE * -1);
+		}
+
+		public static int toScreenInt (int integer)
+		{
+			return integer * SCALE;
+		}
+
+		public static float toScreenFloat (float fl)
+		{
+			return fl * SCALE;
+		}
+	}
+}
+
